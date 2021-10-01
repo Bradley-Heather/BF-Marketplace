b686568bb3d88a2cdc8cdf9597d2b3f67d034bd6
Bradley Heather <bradleymheather@gmail.com>
2021-10-01 14:01:46 -0400

    Removed unnecessary definitions

--- app/PAB.hs
+++ app/PAB.hs
@@ -13,31 +13,20 @@
 module Main where
 
 import           Control.Monad                       (forM_, void, when)
-import           Control.Monad.Freer                 (Eff, Member, interpret, type (~>))
-import           Control.Monad.Freer.Error           (Error)
-import           Control.Monad.Freer.Extras.Log      (LogMsg)
+import           Control.Monad.Freer                 (interpret)
 import           Control.Monad.IO.Class              (MonadIO (..))
 import           Data.Aeson                          (FromJSON, Result (..), fromJSON)
 import           Data.Default                        (Default (..))
 import           Data.Monoid                         (Last (..))
-import           Data.Text                           (Text, pack)
-
-import           Ledger
-import           Ledger.Constraints
-import qualified Ledger.Value                        as Value
 
 import           Plutus.Contract
-import           Plutus.PAB.Effects.Contract         (ContractEffect (..))
 import           Plutus.PAB.Effects.Contract.Builtin (Builtin, SomeBuiltin (..))
 import qualified Plutus.PAB.Effects.Contract.Builtin as Builtin
-import           Plutus.PAB.Monitoring.PABLogMsg     (PABMultiAgentMsg)
 import           Plutus.PAB.Simulator                (SimulatorEffectHandlers)
 import qualified Plutus.PAB.Simulator                as Simulator
-import           Plutus.PAB.Types                    (PABError (..))
 import qualified Plutus.PAB.Webserver.Server         as PAB.Server
-import qualified Plutus.Contracts.Currency           as Currency
 
-import           Wallet.Emulator.Types               (Wallet (..), walletPubKey)
+import           Wallet.Emulator.Types               (Wallet (..))
 import           Wallet.Types                        (ContractInstanceId (..))
 
 import qualified PropertySale                        as PS
@@ -62,6 +51,9 @@ main = void $ Simulator.runSimulationWith handlers $ do
             cid <- Simulator.activateContract w $ PSBuyer ps
             liftIO $ writeFile ("Buyer" ++ show (getWallet w) ++ ".cid") $ show $ unContractInstanceId cid
 
+    void $ liftIO getLine
+    shutdown       
+
 waitForLast :: FromJSON a => ContractInstanceId -> Simulator.Simulation t a
 waitForLast cid =
     flip Simulator.waitForState cid $ \json -> case fromJSON json of
@@ -69,6 +61,7 @@ waitForLast cid =
         _                       -> Nothing
 
 instance Builtin.HasDefinitions PSContracts where
+    getDefinitions = [PSMinter]
     getSchema = \case
         PSMinter        -> Builtin.endpointsToSchemas @PS.PSMintSchema
         PSSeller _      -> Builtin.endpointsToSchemas @PS.PSSellSchema
